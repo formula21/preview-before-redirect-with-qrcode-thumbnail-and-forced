02c37defcd1dc4122cf700ec4ea915faa53204e7
Anweshan Roy Chowdhury <anweshanrc15@gmail.com>
2021-05-24 03:28:46 +0530

    Update plugin.php

--- plugin.php
+++ plugin.php
@@ -1,46 +1,54 @@
 <?php
 /*
 Plugin Name: Preview URL with QR Code and Thumbnail image
-Plugin URI: https://github.com/prog-it/yourls-preview-url-with-qrcode-thumbnail
+Plugin URI: https://github.com/formula21/yourls-preview-url-with-qrcode-thumbnail/
 Description: Preview URLs before you're redirected there with QR code and Thumbnail image
-Version: 1.1
-Author: progit
-Author URI: https://github.com/prog-it
+Version: 1.0
+Author: formula21
+Author URI: https://github.com/formula21
 */
 
-// EDIT THIS
-
 // Character to add to a short URL to trigger the preview interruption
-define( 'PROGIT_PREVIEW_CHAR', '~' );
-
+define( 'formula21_PREVIEW_CHAR', '~' );
 
 // Handle failed loader request and check if there's a ~
-yourls_add_action( 'loader_failed', 'progit_preview_loader_failed' );
+yourls_add_action( 'loader_failed', 'formula21_preview_loader_failed' );
 
-function progit_preview_loader_failed( $args ) {
-	yourls_load_custom_textdomain( 'progit_translation', dirname( __FILE__ ) . '/languages' );
+yourls_add_action('pre_redirect', 'formula21_preview_show_redirect');
+
+function formula21_preview_loader_failed( $args ) {
+	yourls_load_custom_textdomain( 'formula21_translation', dirname( __FILE__ ) . '/languages' );
 	$request = $args[0];
 	$pattern = yourls_make_regexp_pattern( yourls_get_shorturl_charset() );
-	if( preg_match( "@^([$pattern]+)".PROGIT_PREVIEW_CHAR."$@", $request, $matches ) ) {
+	if( preg_match( "@^([$pattern]+)".formula21_PREVIEW_CHAR."$@", $request, $matches ) ) {
 		$keyword = isset( $matches[1] ) ? $matches[1] : '';
 		$keyword = yourls_sanitize_keyword( $keyword );
-		progit_preview_show( $keyword );
+		formula21_preview_show( $keyword );
+		die();
+	}
+}
+
+
+function formula21_preview_show_redirect( $keyword ){
+	if(defined("PRE_REDIRECT_PREVIEW") && PRE_REDIRECT_PREVIEW && defined("PRE_REDIRECT_SECONDS") && is_int(PRE_REDIRECT_SECONDS) && PRE_REDIRECT_SECONDS > 0){
+		// header( "refresh:PRE_REDIRECT_SECONDS+10;url=$longurl);
+		formula21_preview_show($keyword, true);
 		die();
 	}
 }
 
 // Show the preview screen for a short URL
-function progit_preview_show( $keyword ) {
+function formula21_preview_show( $keyword, $preview = false) {
 	require_once( YOURLS_INC.'/functions-html.php' );
 
-	yourls_html_head( 'preview', yourls__('Preview short URL', 'progit_translation') );
+	yourls_html_head( 'preview', yourls__('Preview short URL', 'formula21_translation') );
 	yourls_html_logo();
 
 	$title		= yourls_get_keyword_title( $keyword );
 	$base		= YOURLS_SITE;
 	$shorturl	= "$base/$keyword";
 	$longurl	= yourls_get_keyword_longurl( $keyword );
-	$char  		= PROGIT_PREVIEW_CHAR;
+	$char  		= formula21_PREVIEW_CHAR;
 	// Required this plugin - https://github.com/seandrickson/YOURLS-QRCode-Plugin
 	$qrcode 	= YOURLS_SITE.'/'.$keyword.'.qr';
 	// Required this plugin - https://github.com/prog-it/yourls-thumbnail-url
@@ -106,24 +114,25 @@ function progit_preview_show( $keyword ) {
 		}
 	}		
 	</style>
-	<h2><?php yourls_e('Preview short URL', 'progit_translation'); ?></h2>
+	<h2><?php yourls_e('Preview short URL', 'formula21_translation'); ?></h2>
 	<div class="halves">
 		<div class="half-width thumb-box">
-			<img class="short-thumb" src="<?php echo yourls_esc_url( $thumb ); ?>">
+			<?php $onerror = YOURLS_SITE.'/user/plugins/yourls-preview-url-with-qrcode-thumbnail/image/No-Image-Placeholder.svg' ?>
+			<img class="short-thumb" src="<?php echo yourls_esc_url( $thumb ); ?>" onerror="this.src='<?php echo yourls_esc_url( $onerror );?>'">
 		</div>
 		<div class="half-width desc-box">
 			<div>
-				<?php yourls_e('You requested a shortened URL', 'progit_translation'); ?> <strong><?php echo yourls_esc_url( $shorturl ); ?></strong>
-				<p><?php yourls_e('This URL points to', 'progit_translation'); ?>:</p>		
+				<?php yourls_e($preview==false?'You requested a shortened URL','The preview for the shortened URL ', 'formula21_translation'); ?> <strong><?php echo yourls_esc_url( $shorturl ); ?></strong>
+				<p><?php yourls_e('This URL points to', 'formula21_translation'); ?>:</p>		
 			</div>
 			<div>
-				<?php yourls_e('Long URL', 'progit_translation'); ?> : <strong><a href="<?php echo yourls_esc_url( $shorturl ); ?>"><?php  echo yourls_esc_url( $longurl ); ?></a></strong>
+				<?php yourls_e('Long URL', 'formula21_translation'); ?> : <strong><a href="<?php echo yourls_esc_url( $preview==false?$shorturl:$longurl ); ?>"><?php  echo yourls_esc_url( $longurl ); ?></a></strong>
 			</div>
 			<div>
-				<?php yourls_e('Title', 'progit_translation'); ?>: <strong><?php echo $title; ?></strong>
+				<?php yourls_e('Title', 'formula21_translation'); ?>: <strong><?php echo $title; ?></strong>
 			</div>
 			<div>
-				<?php yourls_e('QR code', 'progit_translation'); ?>:
+				<?php yourls_e('QR code', 'formula21_translation'); ?>:
 				<div>
 					<img class="short-qr" src="<?php echo yourls_esc_url( $qrcode ); ?>">
 				</div>
@@ -131,14 +140,14 @@ function progit_preview_show( $keyword ) {
 		</div>
 	</div>
 	<p>
-		<?php yourls_e('If you still want to visit this URL, please go to', 'progit_translation'); ?> 
+		<?php yourls_e('If you still want to visit this URL, please go to', 'formula21_translation'); ?> 
 		<strong>
-			<a href="<?php echo yourls_esc_url( $shorturl ); ?>"><?php yourls_e('this URL', 'progit_translation'); ?></a>
+			<a href="<?php echo yourls_esc_url( $preview==false?$shorturl:$longurl ); ?>"><?php yourls_e('this URL', 'formula21_translation'); ?></a>
 		</strong>.
 	</p>
 	<hr>
 	<div class="disclaimer">
-		<?php yourls_e('You will be redirected to another page. We are not responsible for the content of this page.', 'progit_translation'); ?>
+		<?php yourls_e('You will be redirected to another page. We are not responsible for the content of this page.', 'formula21_translation'); ?>
 	</div>
 <?php
 	yourls_html_footer();
